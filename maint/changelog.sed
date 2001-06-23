bba8da8484d594dfc1664191f3049caf0b3df0d2
oskuro <oskuro>
2001-06-23 00:07:03 +0000

    Renamed CHANGES to ChangeLog and RELEASE-NOTES to NEWS, modified release scripts accordingly. Added Mark to the sed file.

--- maint/changelog.sed
+++ maint/changelog.sed
@@ -1,8 +1,8 @@
-# $Id: changelog.sed,v 1.1 2001/06/19 03:38:58 dwonis Exp $
+# $Id: changelog.sed,v 1.2 2001/06/23 00:07:03 oskuro Exp $
 # Changes cvs2cl's output to something more pleasant
 s/\([0-9][0-9]:[0-9][0-9]\) /\1 UTC --/g
 
 # Add yourself below:
 s/dwonis/Dwayne C. Litzenberger <dlitz@dlitz.net>/g
 s/oskuro/Jordi Mallach <jordi@sindominio.net>/g
-#s/????/Mark Hurley <debian4tux@telocity.com>/g
+s/markph/Mark Hurley <debian4tux@telocity.com>/g
--- maint/prerelease.sh
+++ maint/prerelease.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: prerelease.sh,v 1.2 2001/06/19 03:52:02 dwonis Exp $
+# $Id: prerelease.sh,v 1.3 2001/06/23 00:07:03 oskuro Exp $
 # wmBiff prerelease script.  Used mainly by the upstream maintainer(s).
 # Run this before making a new release of wmBiff
 # You will need cvs2cl.
@@ -14,7 +14,7 @@ set -e -x
 	make distclean
 )
 
-cat <<EOF > CHANGES
+cat <<EOF > ChangeLog
 CVS Changelog.  Don't bother editing this file, because it will get overwritten
 by maint/prerelease.sh .
 
@@ -23,10 +23,10 @@ CVS on commit.
 
 EOF
 
-echo "  $""Log""$" >> CHANGES
-echo ""            >> CHANGES
+echo "  $""Log""$" >> ChangeLog
+echo ""            >> ChangeLog
 
-cvs2cl --stdout --utc --day-of-week -t | sed -f maint/changelog.sed >> CHANGES
+cvs2cl --stdout --utc --day-of-week -t | sed -f maint/changelog.sed >> ChangeLog
 
 set +x
 
@@ -36,11 +36,11 @@ echo ""
 echo ""
 echo "Pre-release script successful!"
 echo ""
-echo "Don't forget to edit RELEASE-NOTES, and substitute today's date for"
+echo "Don't forget to edit NEWS, and substitute today's date for"
 echo "(unreleased), commit and tag the release, then add a new version line"
 echo "and commit again."
 echo ""
-echo "Also, don't forget to update WMBIFF_VERSION in wmbiff/Makefile ."
+echo "Also, don't forget to update WMBIFF_VERSION in wmbiff/Makefile."
 echo ""
 echo "The date for the release notes is:   `date -R`"
 
