dae13cc33dfb3a542b6e6b85c4f7111c3c6cd79d
bluehal <bluehal>
2001-11-16 00:40:52 +0000

    regex helpers common to pop3 and imap4 clients extracted and moved here

--- wmbiff/charutil.h
+++ wmbiff/charutil.h
@@ -1,12 +1,14 @@
-/* $Id: charutil.h,v 1.3 2001/10/04 09:50:59 jordi Exp $ */
+/* $Id: charutil.h,v 1.4 2001/11/16 00:40:52 bluehal Exp $ */
 /* Author: Mark Hurley  (debian4tux@telocity.com)
  *
- * Simple char util's to trim char string arrays
+ * Character / string manipulation utilities. 
  *
  */
 
 #ifndef CHARUTIL
 #define CHARUTIL
+
+#include <regex.h>
 
 int FullTrim(char *psValue);
 
@@ -15,4 +17,14 @@ void Bin2Hex(unsigned char *src, int length, char *dst);
 void Encode_Base64(char *src, char *dst);
 void Decode_Base64(char *src, char *dst);
 
+/* helper function for the configuration line parser */
+void copy_substring(char *destination,
+					int startidx, int endidx, const char *source);
+
+/* common to Pop3 and Imap4 authentication list grabber. */
+void grab_authList(const char *source, char *destination);
+
+/* handles main regex work */
+int compile_and_match_regex(const char *regex, const char *str,
+							/*@out@ */ struct re_registers *regs);
 #endif
