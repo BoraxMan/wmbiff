db1f25d85ce1eab81cd4c21e4cb6035f1d7245ff
bluehal <bluehal>
2002-08-06 08:05:33 +0000

    seek out and use athe underrated automake 1.6.

--- FromCVS.sh
+++ FromCVS.sh
@@ -7,14 +7,37 @@
 # complains if any macro other than its own specifies
 # the file
 
-if [ -e /usr/share/aclocal/libgnutls.m4 ]; then
-   aclocal;
-else 
-   aclocal -I autoconf;
+# it's skanky that I have to look explicitly for a newer 
+# version to invoke.
+
+if [ -x /usr/bin/automake-1.6 ]; then 
+    echo "Using automake 1.6";
+  AUTOMAKE=automake-1.6
+  ACLOCAL=aclocal-1.6
+else
+  AUTOMAKE=automake
+  ACLOCAL=aclocal
 fi
- autoheader && \
- automake -a && \
- autoconf && \
+
+# debian, at least, handles this one correctly;
+# this hack is for potential redhatters.
+if [ -x /usr/bin/autoconf2.50 ]; then 
+    echo "Using autoconf 2.5x";
+  AUTOCONF=autoconf2.50
+  AUTOHEADER=autoheader2.50
+else
+  AUTOCONF=autoconf
+  AUTOHEADER=autoheader
+fi
+
+if [ -e /usr/share/aclocal/libgnutls.m4 ]; then
+   ${ACLOCAL};
+else 
+   ${ACLOCAL} -I autoconf;
+fi
+ ${AUTOHEADER} && \
+ ${AUTOMAKE} -a && \
+ ${AUTOCONF} && \
  ./configure && \
  make
 
